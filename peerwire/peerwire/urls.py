208983587a60623d0503f9909f17105bead5dde9
Robin Schroer <sulamiification@gmail.com>
2014-02-10 23:22:03 +0100

    general: added login/logout system and dynamic greetings

--- peerwire/peerwire/urls.py
+++ peerwire/peerwire/urls.py
@@ -14,5 +14,12 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'login.html',
+         'redirect_field_name': 'redirect'},
+        name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': '/'},
+        name='logout'),
     url(r'^', include('projects.urls', namespace='projects')),
 )
