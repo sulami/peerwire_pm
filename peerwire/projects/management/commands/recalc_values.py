2ebea12acdd9fdbeb577ced43bb87236419aeb5c
Robin Schroer <sulamiification@gmail.com>
2014-02-12 17:13:15 +0100

    performance tweaks for recalc_values, only one full query now

--- peerwire/projects/management/commands/recalc_values.py
+++ peerwire/projects/management/commands/recalc_values.py
@@ -5,12 +5,13 @@ class Command(BaseCommand):
     help = 'Recalculates values for projects, decrements them at midnight'
 
     def handle(self, *args, **options):
-        # Active projects
-        for p in Project.objects.filter(status=1):
-            p.value *= .9
-            p.save()
-        # Inactive projects
-        for p in Project.objects.filter(status=0):
-            p.value = 0
-            p.save()
+        for p in Project.objects.all():
+            # Active projects
+            if p.status = 1:
+                p.value *= .9
+                p.save()
+            # Inactive projects
+            else:
+                p.value = 0
+                p.save()
 
