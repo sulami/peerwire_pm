8fa210ff7417d27a82967533a9cc7d88cab8cf4f
Robin Schroer <sulamiification@gmail.com>
2014-03-21 14:15:33 +0100

    haystack: actual integration, basic functional search engine

---
+++ peerwire/projects/search_indexes.py
@@ -0,0 +1,12 @@
+import datetime
+from haystack import indexes
+from projects.models import User, Project
+
+class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Project
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
