1ebac3ad1a2961143e67f9fae88c1071b88c18b2
Robin Schroer <sulamiification@gmail.com>
2014-03-24 19:07:00 +0100

    settings, static: introduced static content

--- peerwire/peerwire/settings.py
+++ peerwire/peerwire/settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/sulami/Desktop/peerwire_pm/static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -74,6 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/sulami/build/peerwire_pm/static',
 )
 
 # List of finder classes that know how to find static files in
