af46a0658fd6ca12b402428e0b00f611a6031b27
Robin Schroer <sulamiification@gmail.com>
2014-03-27 10:39:01 +0100

    projects/urls: fixed urls for pks over 9

--- peerwire/projects/urls.py
+++ peerwire/projects/urls.py
@@ -4,6 +4,6 @@ from projects import views
 
 urlpatterns = patterns('projects.views',
     url(r'^$', 'index', name='index'),
-    url(r'p/(\d)/$', 'projectpage', name='projectpage'),
-    url(r'u/(\d)/$', 'profilepage', name='profilepage'),
+    url(r'p/(\d+)/$', 'projectpage', name='projectpage'),
+    url(r'u/(\d+)/$', 'profilepage', name='profilepage'),
     )
