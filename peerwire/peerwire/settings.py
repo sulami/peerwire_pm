9e5dbd0e27657e71f5ae846b41de0269b84ec782
Robin Schroer <sulamiification@gmail.com>
2014-04-09 20:57:07 +0200

    settings: changed caching backend to dummy for development

--- peerwire/peerwire/settings.py
+++ peerwire/peerwire/settings.py
@@ -173,8 +173,9 @@ LOGGING = {
 ### Cache
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'peerwire'
+        # 'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        # 'LOCATION': 'peerwire'
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
 CACHE_MIDDLEWARE_KEY_PREFIX = 'peerwire'
