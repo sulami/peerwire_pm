294494f89ce9146b4c5b1a285631672b3ca04820
Robin Schroer <sulamiification@gmail.com>
2014-04-14 15:47:06 +0200

    forms: added placeholder for project description textarea

--- peerwire/projects/forms.py
+++ peerwire/projects/forms.py
@@ -1,6 +1,11 @@
 from django.forms import ModelForm, BooleanField, CharField, Form
 from projects.models import *
 
+project_ph = """You can use Markdown here
+ (http://daringfireball.net/projects/markdown/syntax). You also might want to
+ include a way to contact the person responsible for this project, like an email
+ address or an irc channel. This help text will not be in the description."""
+
 class UserLangForm(ModelForm):
     class Meta:
         model = UserLang
@@ -27,6 +32,11 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['name', 'description', 'langs', 'skills', 'level', 'status',
             'seeking']
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs['placeholder'] = project_ph
+
 
 class InputForm(Form):
     username = CharField(max_length=100)
