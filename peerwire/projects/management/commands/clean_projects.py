bdc52e86c4b1cdd71f2052c99829a4d967375a1b
Robin Schroer <sulamiification@gmail.com>
2014-04-14 19:04:28 +0200

    commands: added command to delete orphaned projects

---
+++ peerwire/projects/management/commands/clean_projects.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+from projects.models import Project
+
+class Command(BaseCommand):
+    help = 'Deletes projects without owners'
+
+    def handle(self, *args, **options):
+        count = 0
+        for p in Project.objects.all():
+            if p.owners.all().count() == 0:
+                print "Deleting: ", p
+                count += 1
+                p.delete()
+        print "Deleted %d projects." % count
