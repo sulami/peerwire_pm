7f0bb3e3891b88c2a11fdbb438aa9ec3732fb0f5
Robin Schroer <sulamiification@gmail.com>
2014-04-23 02:33:07 +0200

    views: removed unneeded imports

--- peerwire/projects/views.py
+++ peerwire/projects/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth import authenticate, login
 from django.forms.models import modelformset_factory
 from django.core.urlresolvers import reverse
