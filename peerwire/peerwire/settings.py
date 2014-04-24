79cdaaa5aa1ff5dab4a034428ec4207e6dcfbe0e
Robin Schroer <sulamiification@gmail.com>
2014-04-24 02:23:23 +0200

    settings: added static/media dirs

--- peerwire/peerwire/settings.py
+++ peerwire/peerwire/settings.py
@@ -56,7 +56,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/sulami/build/peerwire_pm/media'
+MEDIA_ROOT = '/home/sulami/html/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -67,7 +67,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/sulami/html/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
